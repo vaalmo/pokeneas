4392f0bd143c037a5165d4b83e13e10caad90c48
vaalmo <valentinamoralesvillada17@outlook.com>
2024-11-08 13:25:10 -0500

    (add): added mock pokenea array

---
+++ data/pokeneas.js
@@ -0,0 +1,28 @@
+const pokeneas = [
+    {
+      id: 1,
+      name: "mimi",
+      height: "1",
+      ability: "fuego",
+      image: "xd",
+      phrase: "la bna mi so",
+    },
+    {
+      id: 2,
+      name: "momo",
+      height: "2",
+      ability: "agua",
+      image: "xd2",
+      phrase: "mas q mi bro eres mi brocoli",
+    },
+    {
+      id: 3,
+      name: "meme",
+      height: "3",
+      ability: "tierra",
+      image: "xd3",
+      phrase: "ay nooo eso si jamaaaaas",
+    }
+  ];
+  
+export default pokeneas;
